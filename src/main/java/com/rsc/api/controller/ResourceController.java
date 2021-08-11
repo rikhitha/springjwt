410b8013df470de98064191c9dd3ec3d303c6848
rikhitha <45781265+rikhitha@users.noreply.github.com>
2021-08-11 13:15:32 +0530

    Add files via upload

---
+++ src/main/java/com/rsc/api/controller/ResourceController.java
@@ -0,0 +1,47 @@
+package com.rsc.api.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.rsc.api.model.Country;
+import com.rsc.api.service.CountryService;
+
+// @CrossOrigin(origins = "http://localhost:3000", allowedHeaders = "*")
+// @CrossOrigin(origins = "http://localhost:3000")
+//@CrossOrigin(origins = "*", allowedHeaders = "*")
+@RestController
+public class ResourceController extends BaseController {
+	
+	@Autowired
+	private CountryService service;
+	
+	// @CrossOrigin(origins = "http://localhost:3000")
+	@RequestMapping("/hellouser")
+	public String getUser()
+	{
+		return "Hello User";
+	}
+	
+	// @CrossOrigin(origins = "http://localhost:3000")
+	@RequestMapping(value = "/helloadmin", method = RequestMethod.GET)
+	public String getAdmin()
+	{
+		return "Hello Admin";
+	}
+	
+	// @CrossOrigin(origins = "http://localhost:3000")
+	@RequestMapping("/countries")
+	public List<Country> getCountries()
+	{
+		System.out.println(this.getUserContext());
+		return service.getCountries();
+	}
+	
+	
+}
